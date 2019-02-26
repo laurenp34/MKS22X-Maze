c6c3137ef3a48b38b2a33e3d4bf0f81dddf0773f
Lauren Pehlivanian <lpehlivanian00@stuy.edu>
2019-02-26 10:10:18 -0500

    rows cols to keep track of char[][] lengths, next step is to make loop to loop through mazeString

--- Maze.java
+++ Maze.java
@@ -3,28 +3,42 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Maze {
-  private String maze;
+  private char[][] maze;
 
   public Maze(String fileName) {
+    String mazeString = "";
+    int rows = 0;
+    int cols = 0;
     try {
       File file = new File(fileName);
       Scanner s = new Scanner(file);
 
       while (s.hasNextLine()) {
         String line = s.nextLine();
-        maze += line;
-        System.out.println(line);
+        rows++;
+        cols = line.length();
+        mazeString += line;
+        //System.out.println(line);
       }
 
     } catch (FileNotFoundException e) {
       System.out.println("File not found: "+fileName);
       System.exit(1);
     }
+
+    System.out.println(mazeString);
+    maze = new char[rows][cols];
+
+    int r=0;
+    int c=0;
+    while ()
+
   }
 
   public static void main(String[] args) {
     Maze m = new Maze("Maze1.txt");
-    
+
+
 
   }
 
