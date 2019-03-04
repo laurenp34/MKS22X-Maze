426aa5a8df11fe8b887eb992b42ff1999422e326
Lauren Pehlivanian <lpehlivanian00@stuy.edu>
2019-03-04 08:45:58 -0500

    copied from website

--- Driver.java
+++ Driver.java
@@ -3,10 +3,26 @@ import java.util.*;
 
 public class Driver{
     public static void main(String[]args){
-      String filename = "data1.dat";
+      String filename1 = "data1.dat";
+      String filename2 = "data2.dat";
+      String filename3 = "data3.dat";
       //try{
         Maze f;
-        f = new Maze(filename);//true animates the maze.
+        f = new Maze(filename1);//true animates the maze.
+
+        f.setAnimate(true);
+        f.solve();
+        System.out.println(f);
+
+        Maze f;
+        f = new Maze(filename2);//true animates the maze.
+
+        f.setAnimate(true);
+        f.solve();
+        System.out.println(f);
+
+        Maze f;
+        f = new Maze(filename3);//true animates the maze.
 
         f.setAnimate(true);
         f.solve();
